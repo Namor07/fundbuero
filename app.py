9e4fa6771f959d869f4413339afdec22107c0292
Namor07 <romanhunzinger@gmail.com>
2026-02-26 20:03:06 +0100

    Update model path to keras_model.h5

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-MODEL_PATH = "model/model.h5"
+MODEL_PATH = "model/keras_model.h5"
 LABELS_PATH = "labels.txt"
 
 # 🔑 WICHTIG: compile=False
